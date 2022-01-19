89263cbab71a81a77485d6860a697cf5cb9441af
akazwz <akazwz@pm.me>
2022-01-19 23:26:57 +0800

    update index

--- pages/index.tsx
+++ pages/index.tsx
@@ -11,13 +11,13 @@ const Home: NextPage = () => {
         <meta name="description" content="file server by next.js" />
         <link rel="icon" href={'/favicon.ico'} />
       </Head>
-
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="#">File Server!</a>
+          Welcome to File Server
+          <hr/>
+          <a href={'/upload'}>START!</a>
         </h1>
       </main>
-
       <footer className={styles.footer}>
         <a
           href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
