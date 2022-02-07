fa4902a510031ccb2a7cc2de15e235460dd8365c
akazwz <akazwz@pm.me>
2022-02-08 01:31:16 +0800

    update sequelize

--- pages/api/mysql.ts
+++ pages/api/mysql.ts
@@ -1,20 +1,10 @@
 import { NextApiRequest, NextApiResponse } from 'next'
-import { Sequelize } from 'sequelize'
-import mysql2 from 'mysql2'
 import { runCors } from '../../src/middleware/cors'
+import { getSequelize } from '../../src/lib/sequelize'
 
 export default async function handle (req: NextApiRequest, res: NextApiResponse) {
   await runCors(req, res)
-  const sequelize = new Sequelize(
-    process.env.MYSQL_DB || '',
-    process.env.MYSQL_USERNAME || '',
-    process.env.MYSQL_PASSWORD || '',
-    {
-      host: process.env.MYSQL_HOST,
-      port: Number(process.env.MYSQL_PORT),
-      dialect: 'mysql',
-      dialectModule: mysql2,
-    })
+  const sequelize = getSequelize()
   try {
     await sequelize.authenticate()
     res.status(200).json({ msg: 'connect success' })
--- src/lib/sequelize.ts
+++ src/lib/sequelize.ts
@@ -1,1 +1,15 @@
-export default {}
+import { Sequelize } from 'sequelize'
+import mysql2 from 'mysql2'
+
+export const getSequelize = () => {
+  return new Sequelize(
+    process.env.MYSQL_DB || '',
+    process.env.MYSQL_USERNAME || '',
+    process.env.MYSQL_PASSWORD || '',
+    {
+      host: process.env.MYSQL_HOST,
+      port: Number(process.env.MYSQL_PORT),
+      dialect: 'mysql',
+      dialectModule: mysql2,
+    })
+}
