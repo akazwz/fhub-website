db36c716251c495020231e0d1aae41834f4b2569
akazwz <akazwz@pm.me>
2022-02-07 17:57:18 +0800

    rename dic

--- pages/dashboard/file/index.tsx
+++ pages/dashboard/file/index.tsx
@@ -5,7 +5,7 @@ import {
   Grid,
 } from '@chakra-ui/react'
 import { DashBoardLayout } from '../../../src/components/dashboard/layout'
-import FileCard, { ICloudFile } from '../../../src/components/files/FileCard'
+import FileCard, { ICloudFile } from '../../../src/components/file/FileCard'
 
 const File = () => {
   const files: ICloudFile[] = [
